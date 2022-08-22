69c2cef9a34784c78dcab028d2645ade930b30a3
khangnm123 <khangpro2536@gmail.com>
2022-08-23 01:27:34 +0700

    first commit

---
+++ bo1.js
@@ -0,0 +1,60 @@
+console.log("Bài 1")
+console.log("JavaScript – “I love you”")
+
+console.log("======")
+console.log("Bài 2")
+var a = 3, b=2;
+console.log("Vậy a/b = " ,a/b);
+
+console.log("======")
+console.log("Bài 3")
+var a = 3, b=2;
+console.log("Vậy a + b = " ,a + b);
+
+console.log("======")
+console.log("Bài 4")
+var F= 32;
+console.log("công thức chuyển là: " , C=5/9*(F-32));
+
+console.log("======")
+console.log("Bài 5")
+var a = 2, b=4;
+console.log("Vậy a^2 + b^2 = " ,a**2 + b**2);
+
+console.log("======")
+
+console.log("Bài 6")
+var a = 2, b=4;
+console.log("Vậy (a+b)^2 = " ,(a+b)**2);
+
+console.log("======")
+console.log("Bài 7")
+var a=2 , b=4 , c=16;
+console.log("Vậy a^2+b^2/c = " , (a**2*b**2)/(c));
+
+console.log("======")
+console.log("Bài 8")
+var toán= 1 , lý = 2,sinh = 3, anh =4;
+console.log("điểm trung bình của 4 môn : ",(toán+lý+anh+sinh)/4);
+
+console.log("======")
+console.log("Bài 9")
+var a = 3, b=0;
+console.log("Vậy a/b= " , a/b)
+
+console.log("======")
+console.log("Bài 10")
+console.log("Vậy Nghiệm phương trình bậc 2 là : " , 6 / 6);
+
+console.log("======")
+console.log("Bài 11")
+var x = 4 , y =2;
+console.log("Tính S : " , (Math.sqrt(x)+ Math.abs(x))/Math.sqrt(x**y));     
+console.log("Sau khi làm tròn: " ,Math.floor(1.25));
+
+console.log("======")
+console.log("Bài 12")
+var arr = new Array(1 , 3 , 7 , 2 , 5);
+ arr[3];
+ console.log(arr[4]);
+
